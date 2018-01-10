b47841434689ae5c1070ab55df4b5dcf5cbb52d0
huang7230468 <417213902@qq.com>
2018-01-10 21:58:13 +0800

    test

--- dubbo-api/src/main/java/ant/dubbo/api/UserService.java
+++ dubbo-api/src/main/java/ant/dubbo/api/UserService.java
@@ -1,5 +1,9 @@
 package ant.dubbo.api;
 
+/**
+ * @author
+ * @create 2018-01-10 21:57
+ **/
 public interface UserService {
-        public String getName(String name);
+    public String getName(String name);
 }
